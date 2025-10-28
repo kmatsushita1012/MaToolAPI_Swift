86064d37331fa5f529ba45c16dc8046f9b770cdf
kmatsushita1012 <kmatsushita1012@icloud.com>
2025-10-28 13:48:47 +0900

    maintテスト#13

--- scripts/build-swift.sh
+++ scripts/build-swift.sh
@@ -1,80 +1,33 @@
 #!/bin/bash
+set -eux
 
-## Install and Remove conflicting dependencies 
+# Install dependencies (no sudo, running as root)
+dnf install -y python3-pip git make gcc gcc-c++ cmake clang ninja-build \
+  texinfo gmp-devel mpfr-devel libicu-devel libxml2-devel libbsd-devel \
+  libuuid-devel libedit-devel sqlite-devel libcurl-devel \
+  pkgconfig zlib-devel ncurses-devel tzdata which wget unzip zip tar \
+  rsync bison procps-ng swig
 
-sudo dnf install python3-pip -y
-sudo dnf remove python3-requests python-urllib3 -y
-
-## Install LD GOLD 
-sudo dnf install gmp-devel mpfr-devel texinfo bison git gcc-c++ -y
-mkdir ld.gold && cd ld.gold
+# Build ld.gold (optional)
+mkdir -p /tmp/ld.gold && cd /tmp/ld.gold
 git clone --depth 1 git://sourceware.org/git/binutils-gdb.git binutils
 mkdir build && cd build
 ../binutils/configure --enable-gold --enable-plugins --disable-werror
-make all-gold
-cd gold
-make all-am
-cd ..
+make all-gold -j$(nproc)
 cp gold/ld-new /usr/bin/ld.gold
-cd ~
-/usr/bin/ld.gold -v
+ld.gold -v
+cd /
 
-## Install Swift build dependencies
-sudo dnf -y install     \
-  clang            \
-  cmake            \
-  curl-devel       \
-  gcc-c++          \
-  git              \
-  glibc-static     \
-  libbsd-devel     \
-  libedit-devel    \
-  libicu-devel     \
-  libuuid-devel    \
-  libxml2-devel    \
-  ncurses-devel    \
-  ninja-build      \
-  python3-pexpect  \
-  pkgconfig        \
-  procps-ng        \
-  python           \
-  python3-devel    \
-  python3-six      \
-  python3-psutil   \
-  python3-pkgconfig \
-  rsync            \
-  sqlite-devel     \
-  swig             \
-  tzdata           \
-  unzip            \
-  uuid-devel       \
-  wget             \
-  which            \
-  zip
-
-mkdir swift-project && cd swift-project   
-git clone https://github.com/apple/swift.git swift
+# Build Swift toolchain
+mkdir -p /tmp/swift-project && cd /tmp/swift-project
+git clone https://github.com/apple/swift.git
 cd swift
 ./utils/update-checkout --clone
 
-## Create an EBS snapshot or an AMI at this point because the above takes ~30 minutes
-
-## Build Instructions 
-
-## Specify the release you want to build
+# Set version
 SWIFT_VERSION=6.2
-
 ./utils/update-checkout --scheme release/$SWIFT_VERSION
 
-## IMPORTANT : to build a version >= 5.9, you must install swift 5.8 and make sure `swift` is in the PATH
-
-## Building 
-# simple build
-# ./utils/build-script --release-debuginfo --skip-early-swift-driver
-# or 
-# build and package
-./utils/build-script --preset=buildbot_linux,no_assertions,no_test install_destdir=/tmp installable_package=/tmp/swift-${SWIFT_VERSION}-amazonlinux2023.tar.gz
-
-## Cleaning in between builds
-rm -rf ../build
-rm -rf /tmp/usr 
+# Build (takes a few hours)
+./utils/build-script --preset=buildbot_linux,no_assertions,no_test \
+  install_destdir=/tmp installable_package=/tmp/swift-${SWIFT_VERSION}-amazonlinux2023.tar.gz
